0e44eb2bfc0ff522f35126f3905ba9ec507d219b
Kareem Yakubu <kareem.717@icloud.com>
2025-06-01 20:41:43 -0400

    feat(landing): add app download link via api
    closes: #24

--- apps/landing/src/components/download-link.tsx
+++ apps/landing/src/components/download-link.tsx
@@ -1,35 +1,36 @@
+import { createClient } from "@qahwa/sdk";
 import { buttonVariants } from "@qahwa/ui/components/button";
 import { cn } from "@qahwa/ui/lib/utils";
 
-const PLATFORMS = ["darwin/arm64", "darwin/x64"] as const;
+const PLATFORMS = ["darwin"] as const;
+const ARCHS = ["arm64"] as const;
 
 export type Platform = (typeof PLATFORMS)[number];
+export type Arch = (typeof ARCHS)[number];
 
 interface DownloadLinkProps extends React.ComponentProps<"a"> {
   platform: Platform;
+  arch: Arch;
 }
 
 export function DownloadLink({
   children = "Download",
   platform,
+  arch,
   onClick,
   ...props
 }: DownloadLinkProps) {
-  let url = "";
-  switch (platform) {
-    case "darwin/arm64":
-      url = `${import.meta.env.VITE_RELEASE_S3_ENDPOINT}/releases/latest-darwin-arm64.zip`;
-      break;
-    default:
-      throw new Error(`Unsupported platform: ${platform}`);
-  }
+  const api = createClient(import.meta.env.VITE_API_URL);
+
+  const url = api.download[":platform"][":arch"].$url({
+    param: {
+      platform,
+      arch,
+    },
+  });
 
   return (
-    <a
-      href={url.toString()}
-      {...props}
-      className={cn(buttonVariants(), props.className)}
-    >
+    <a href={url.toString()} {...props} className={cn(buttonVariants(), props.className)}>
       {children}
     </a>
   );
